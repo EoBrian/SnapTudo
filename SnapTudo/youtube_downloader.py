d455731abcf6fb979ab97bf1b6f5d9b83169cbee
Brian Ferreira <101906190+EoBrian@users.noreply.github.com>
2022-06-04 13:33:41 -0300

    Update youtube_downloader.py

--- SnapTudo/youtube_downloader.py
+++ SnapTudo/youtube_downloader.py
@@ -1,30 +1,44 @@
 import apps
 import PySimpleGUI as sg
 
+#---------------------------------------------------------------------------------------------------------------------------------
 
 layout = [
-    [sg.Text('LINK:'), sg.Input('', key='-LINK-'), sg.Button('VERIFICAR',key='-BUTTON-')],
+    [sg.Text('LINK:',background_color='dimgray'), sg.Input('', key='-LINK-'), sg.Button('VERIFICAR',key='-BUTTON-')],
     
-    [sg.Text('TITLE:',visible=False,key='text1'), sg.Text('', key='-TITLE-',visible=False)],
+    [sg.Text('TITLE:',visible=False,key='text1',background_color='dimgray'), sg.Text('', key='-TITLE-',visible=False)],
     
-    [sg.Text('DESCRIPTION:',visible=False,key='text2')],
+    [sg.Text('DESCRIPTION:',visible=False,key='text2',background_color='dimgray')],
     [sg.Multiline('', key='-DESCRIPTION-',visible=False,size=(50,10),expand_x=True)],
 
-    [sg.Text('VIDEO',expand_x=True), sg.Combo(values=('360p','480p','720p'),key='-VIDEO-'),
-        sg.Text('AUDIO',expand_x=True), sg.Combo(values=('Median Quality','High Quality'),key='-AUDIO-'),],
+    [sg.Text('VIDEO',expand_x=True,background_color='dimgray',visible=False, key='text3'),
+     sg.Combo(values=('360p','480p','720p'),key='-VIDEO-',size=10,visible=False),
+
+            sg.Text(' ' * 30, justification='center', background_color='dimgray',visible=False,key='text0') ,
+
+        sg.Text('AUDIO', expand_x=True, justification='right',visible=False, background_color='dimgray', key='text4'),
+         sg.Combo(values=('Median Quality','High Quality'),key='-AUDIO-',visible=False)],
     
-    [sg.Button('DOWNLOAD',expand_x=True)],
+    [sg.Button('DOWNLOAD',expand_x=True,visible=False)],
 ]
+#---------------------------------------------------------------------------------------------------------------------------------
 
-sg.theme('DarkRed1')
+sg.theme_background_color('dimgray')
+sg.theme_text_element_background_color('black')
 
-WINDOW = sg.Window('SnapTudo', layout=layout, icon= 'icon.ico')
+#---------------------------------------------------------------------------------------------------------------------------------
+
+WINDOW = sg.Window('SnapTudo', layout=layout, icon='icon.ico',
+                     font='Helvetica', button_color='red')
+
+#---------------------------------------------------------------------------------------------------------------------------------
 
 while True:
     event, values = WINDOW.read()
 
     if event == sg.WIN_CLOSED:
         break
+    #--------------------------------------------------------------------------------------------------------------------------------
 
     if event == '-BUTTON-':
         try:
@@ -35,8 +49,16 @@ while True:
             WINDOW['-TITLE-'].Update(INFORMAÇÕES['title'],visible=True)
             WINDOW['-DESCRIPTION-'].Update(INFORMAÇÕES['description'],visible=True)
 
+            WINDOW['text3'].Update(visible=True)
+            WINDOW['-VIDEO-'].Update(visible=True)
+            WINDOW['text0'].Update(visible=True)
+            WINDOW['text4'].Update(visible=True)
+            WINDOW['-AUDIO-'].Update(visible=True)
+            WINDOW['DOWNLOAD'].Update(visible=True)
+
         except:
-            WINDOW['-DESCRIPTION-'].Update('ERRO! VERIFIQUE O LINK...')
+            WINDOW['-DESCRIPTION-'].Update('ERRO! VERIFIQUE O LINK...', visible=True)
+    #--------------------------------------------------------------------------------------------------------------------------------
 
     if event == 'DOWNLOAD':
         try:
@@ -60,5 +82,6 @@ while True:
                 values[items] = None
         except:
             WINDOW['-DESCRIPTION-'].Update('ERRO! AO BAIXAR...', visible=True)
+#---------------------------------------------------------------------------------------------------------------------------------
 
 WINDOW.close()
